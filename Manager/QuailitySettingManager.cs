61db22bc0fa0b572f32b155881290e535e01355f
Bazzi4263 <101442013+Bazzi4263@users.noreply.github.com>
2026-01-31 18:48:02 +0900

    Add files via upload

---
+++ Manager/QuailitySettingManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+using UnityEngine.Rendering.PostProcessing;
+
+public class QuailitySettingManager : MonoBehaviour
+{
+    // 성능별로 만들어진 PipelineAsset List
+    [SerializeField]
+    List<RenderPipelineAsset> RenderPipelineAssets;
+
+    public void SetPipeline(int value)
+    {
+        QualitySettings.SetQualityLevel(2 - value);
+        QualitySettings.renderPipeline = RenderPipelineAssets[value];
+        if (value == 2)
+        {
+            var volumes = FindObjectsOfType<Volume>();
+            if (volumes != null)
+            {
+                foreach (var volume in volumes)
+                {
+                    if (volume.profile.TryGet<UnityEngine.Rendering.HighDefinition.Bloom>(out var component))
+                        component.active = false;
+
+                    if (volume.profile.TryGet<UnityEngine.Rendering.HighDefinition.AmbientOcclusion>(out var component2))
+                        component2.active = false;
+
+                    if (volume.profile.TryGet<UnityEngine.Rendering.HighDefinition.MotionBlur>(out var component3))
+                        component3.active = false;
+
+                    if (volume.profile.TryGet<ShadowsMidtonesHighlights>(out var component4))
+                        component4.active = false;
+                }
+            }
+        }
+        else
+        {
+            var volumes = FindObjectsOfType<Volume>();
+            if (volumes != null)
+            {
+                foreach (var volume in volumes)
+                {
+                    if (volume.profile.TryGet<UnityEngine.Rendering.HighDefinition.Bloom>(out var component))
+                        component.active = true;
+
+                    if (volume.profile.TryGet<UnityEngine.Rendering.HighDefinition.AmbientOcclusion>(out var component2))
+                        component2.active = true;
+
+                    if (volume.profile.TryGet<UnityEngine.Rendering.HighDefinition.MotionBlur>(out var component3))
+                        component3.active = true;
+
+                    if (volume.profile.TryGet<ShadowsMidtonesHighlights>(out var component4))
+                        component4.active = true;
+                }
+            }
+        }
+    }
+}
---
+++ Other/PostProcessSettings.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+
+public class PostProcessSettings : MonoBehaviour
+{
+    void Start()
+    {
+        if (QualitySettings.GetQualityLevel() == 0)
+        {
+            var volume = GetComponent<Volume>();
+
+            if (volume.profile.TryGet<UnityEngine.Rendering.HighDefinition.Bloom>(out var component))
+                component.active = false;
+
+            if (volume.profile.TryGet<UnityEngine.Rendering.HighDefinition.AmbientOcclusion>(out var component2))
+                component2.active = false;
+
+            if (volume.profile.TryGet<UnityEngine.Rendering.HighDefinition.MotionBlur>(out var component3))
+                component3.active = false;
+
+            if (volume.profile.TryGet<ShadowsMidtonesHighlights>(out var component4))
+                component4.active = false;
+        }
+    }
+}
